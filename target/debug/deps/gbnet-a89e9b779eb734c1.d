0d2cc289efed523caea1e243f017983a785cb1c6
Dev <devontomlin97@gmail.com>
2025-05-20 22:28:50 -0400

    nit name

---
+++ target/debug/deps/gbnet-a89e9b779eb734c1.d
@@ -0,0 +1,7 @@
+/Users/devontomlin/Documents/repos/gbnet/target/debug/deps/libgbnet-a89e9b779eb734c1.rmeta: src/lib.rs
+
+/Users/devontomlin/Documents/repos/gbnet/target/debug/deps/libgbnet-a89e9b779eb734c1.rlib: src/lib.rs
+
+/Users/devontomlin/Documents/repos/gbnet/target/debug/deps/gbnet-a89e9b779eb734c1.d: src/lib.rs
+
+src/lib.rs:
--- target/debug/deps/gbnet_derive-7aa69c34497f9d7e.d
+++
@@ -1,5 +0,0 @@
-/Users/devontomlin/Documents/repos/gbnet/target/debug/deps/libgbnet_derive-7aa69c34497f9d7e.dylib: gbnet_derive/src/lib.rs
-
-/Users/devontomlin/Documents/repos/gbnet/target/debug/deps/gbnet_derive-7aa69c34497f9d7e.d: gbnet_derive/src/lib.rs
-
-gbnet_derive/src/lib.rs:
